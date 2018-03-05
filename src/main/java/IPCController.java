11d05d1f6e542992be378eb4168623f473f16324
Minibrams <anders@brams.dk>
2018-03-05 02:03:17 +0100

    Initial commit

---
+++ src/main/java/IPCController.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class IPCController {
+    private BufferedReader _reader;
+    private BufferedWriter _writer;
+
+    public IPCController(String executable) {
+        
+    }
+}
